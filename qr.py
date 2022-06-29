cb67b02cf1c04ad285c129c99bbb2f0542ffc2d1
prairiepavement <84539524+prairiepavement@users.noreply.github.com>
2022-06-29 16:24:53 -0500

    edit help menu, build error correction flag convertor and qr code function

--- qr.py
+++ qr.py
@@ -1,8 +1,7 @@
 # qr.py
 
 import argparse
-from PIL import Image
-from pyqrcode import QRCode
+import qrcode
 
 
 def read_user_cli_args():
@@ -13,37 +12,106 @@ def read_user_cli_args():
         argparse.Namespace: Populated namespace object
     """
     parser = argparse.ArgumentParser(
-        description="gets URL to generate QR code"
+        description="gets URL to generate QR code",
+        formatter_class=argparse.RawTextHelpFormatter
     )
     
     parser.add_argument(
         "url", 
         type=str, 
-        help="enter url"
+        help="Enter the URL you would like to convert to a QR code."
     )
 
     parser.add_argument(
-        "version", 
-        choices=range(1, 41), 
+        "-v", 
+        choices=[1, 5, 10, 20, 40], 
         default=1,
         type=int, 
         nargs='?',
-        help="enter version"
+        help="""
+            Enter the version of QR code you desire.
+            The 'version' indicates the size, or number of modules, of a QR code. 
+            A larger version contains greater data, along with increasing the size of the QR code.
+
+            The default is set to the standard version 1, or 21 x 21 modules.
+            """
     )
 
     parser.add_argument(
-        "error_correction", 
-        choices=['-L','-M','-Q','-H'], 
-        default='-M',
+        "-e", 
+        choices=['L','M','Q','H'], 
+        default='M',
         type=str, 
         nargs='?',
-        help="enter error correction"
+        help="""
+            Enter the level of error correction for the QR code you desire.
+            Error correction allows for the capability to restore data should the QR code become damaged or dirty. 
+            There exist four error correction levels to choose from. Raising the level increases the error correction capability but also increases the amount of data.
+
+
+            ERROR CORRECTION LEVEL L:
+                Error Correction: About 7%% or less errors can be corrected.
+                Usage: General marketing purposes.
+
+            ERROR CORRECTION LEVEL M:
+                Error Correction: About 15%% or less errors can be corrected.
+                Usage: General marketing purposes.
+            
+            ERROR CORRECTION LEVEL Q:
+                Error Correction: About 25%% or less errors can be corrected.
+                Usage: Industrial.
+
+            ERROR CORRECTION LEVEL H:
+                Error Correction: About 30%% or less errors can be corrected.
+                Usage: Industrial.
+
+
+            The default is set to the standard error correction level, M.
+            """
     )
 
     return parser.parse_args()
 
 
+def convert_error_correction(error):
+    if user_args.e == "L":
+        user_args.e = qrcode.constants.ERROR_CORRECT_L
+        return
+    elif user_args.e == "M":
+        user_args.e = qrcode.constants.ERROR_CORRECT_M
+        return
+    elif user_args.e == "Q":
+        user_args.e = qrcode.constants.ERROR_CORRECT_Q
+        return
+    else:
+        user_args.e = qrcode.constants.ERROR_CORRECT_H
+        return
+
+
+def build_qr_code(url, version, error):
+    qr = qrcode.QRCode(
+        version=version,
+        error_correction=error,
+    )
+
+    qr.add_data(url)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="black", back_color="white")
+    
+    filename = input("Save file as: ")
+
+    if filename.endswith(".jpg") or filename.endswith(".png"):
+        img.save(filename)
+    else:
+        raise ValueError("Please enter .jpg or .png file type")
+    
+    return 
+
+
+
 
 if __name__ == "__main__":
     user_args = read_user_cli_args()
-    print(user_args.url, user_args.version, user_args.error_correction)
+    convert_error_correction(user_args.e)
+    build_qr_code(user_args.url, user_args.v, user_args.e)
